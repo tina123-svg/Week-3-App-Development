4199b858be3aad59f196f2a919f21e29b3434fd8
tina123-svg <tinasitaula835@gmail.com>
2025-11-11 12:03:01 +0545

    Task 4

---
+++ Task  4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number (1–7): ");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+        switch (dayNumber)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
+        }
+    }
+}
