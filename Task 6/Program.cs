46b18328b9e42020fc07f0bcd47a89d39c9729f8
tina123-svg <tinasitaula835@gmail.com>
2025-11-11 12:07:58 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            int number = Convert.ToInt32(input);
+            Console.WriteLine("You entered: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number format");
+        }
+        finally
+        {
+            Console.WriteLine("Program Executed");
+        }
+
+        Console.WriteLine("----------------------------------------");
+
+        try
+        {
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+            else
+            {
+                Console.WriteLine("Password satisfied the requirement");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
