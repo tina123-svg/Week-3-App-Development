69af57924db0d899a1649e57aff1ce86d25666cb
tina123-svg <tinasitaula835@gmail.com>
2025-11-11 11:49:00 +0545

    Task 2

---
+++ Task2/NullOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class NullOperations
+{
+    public void PerformNullChecks()
+    {
+        string username = null;
+
+        string message = (username == null) ? "Username is not available" : username;
+        Console.WriteLine(message);
+
+        Console.WriteLine(username ?? "Username is not available (using ?? operator)");
+
+        username ??= "DefaultUser";
+        Console.WriteLine("Updated Username: " + username);
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        NullOperations nullOps = new NullOperations();
+        nullOps.PerformNullChecks();
+    }
+}
