05e1b2f9322fbc6d2734aeb114de27d9dc039aec
tina123-svg <tinasitaula835@gmail.com>
2025-11-11 11:59:09 +0545

    Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter yor age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age < 13)
+{
+    Console.WriteLine("Child");
+}
+else if (age >= 13 && age < 19)
+{
+    Console.WriteLine("Teenager");
+}
+else
+{
+    Console.WriteLine("Adult");
+}
