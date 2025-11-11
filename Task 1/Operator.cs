db62a4a439aa192548a124e0371fb61c955a0882
tina123-svg <tinasitaula835@gmail.com>
2025-11-11 11:33:44 +0545

    Task One

---
+++ Task 1/Operator.cs
@@ -0,0 +1,29 @@
+﻿public class Operator
+{
+    public void Add(int a, int b)
+    {
+        Console.WriteLine($"The sum of {a} and {b} is: {a + b}");
+    }
+
+    public void Subtract(int a, int b)
+    {
+        Console.WriteLine($"The difference of {a} and {b} is: {a - b}");
+
+    }
+
+    public void Multiply(int a, int b)
+    {
+        Console.WriteLine($"The product of {a} and {b} is: {a * b}");
+    }
+
+    public void Divide(int a, int b)
+    {
+        Console.WriteLine($"The division of {a} and {b} is: {a / b}");
+    }
+
+    public void OddEvenFinder(int number)
+    {
+        string result = (number % 2 == 0) ? "Even" : "Odd";
+        Console.WriteLine(result);
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+{
+    static void Main(String[] args)
+    {
+        Operator ops = new Operator();
+
+        ops.Add(10, 2);
+        ops.Subtract(10, 2);
+        ops.Multiply(10, 2);
+        ops.Divide(10, 2);
+        ops.OddEvenFinder(10);
+    }
+}
